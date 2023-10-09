820b747cec3a3b8ba407071581882a0afa0fac74
Junior Hirwa <iamhirwejr@gmail.com>
2023-10-09 11:43:01 +0200

    feat: change Link to buttons and use setSearchParams to filter

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -22,10 +22,10 @@ export default function Vans(){
         <>
         <h1 className="text-5xl p-4 font-semibold text-center">Explore our van options 🚐</h1>
         <div className="ml-3">
-            <Link to="?type=simple" className="p-2 ml-1 bg-white font-semibold rounded-md hover:bg-primary hover:text-white hover:cursor-pointer">Simple</Link>
-            <Link to="?type=rugged" className="p-2 ml-1 bg-white font-semibold rounded-md hover:bg-darkGreen hover:text-white hover:cursor-pointer">Rugged</Link>
-            <Link to="?type=luxury" className="p-2 ml-1 bg-white font-semibold rounded-md hover:bg-black hover:text-white hover:cursor-pointer">Luxury</Link>
-            <Link to="." className="p-2 ml-1 bg-white font-semibold rounded-md hover:bg-darkGray hover:text-white hover:cursor-pointer">Clear Filter</Link>
+            <button onClick={()=> setSearchParams({type: "simple"})} className="p-2 ml-1 bg-white font-semibold rounded-md hover:bg-primary hover:text-white hover:cursor-pointer">Simple</button>
+            <button onClick={()=> setSearchParams({type: "rugged"})} className="p-2 ml-1 bg-white font-semibold rounded-md hover:bg-darkGreen hover:text-white hover:cursor-pointer">Rugged</button>
+            <button onClick={()=> setSearchParams({type: "luxury"})} className="p-2 ml-1 bg-white font-semibold rounded-md hover:bg-black hover:text-white hover:cursor-pointer">Luxury</button>
+            <button onClick={()=> setSearchParams({})} className="p-2 ml-1 bg-white font-semibold rounded-md hover:bg-darkGray hover:text-white hover:cursor-pointer">Clear Filter</button>
         </div>
             <article className="mt-10 mb-5 grid grid-cols-1 md:grid-cols-3 place-items-center gap-6 bg-secondary p-10">
                 {displayedVans && displayedVans.map(van => {
