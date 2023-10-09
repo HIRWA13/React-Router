4bed848863f653b1b585afbf3a921cc4cfc103cc
Junior Hirwa <iamhirwejr@gmail.com>
2023-10-09 10:41:42 +0200

    feat: add a search param functionality in the vans component

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,9 +1,15 @@
 import { useState, useEffect } from "react";
 import Van from "../../components/Van/Van";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 
 export default function Vans(){
     const [vans, setVans] = useState([]);
+   const [searchParams, setSearchParams] = useSearchParams();
+   const typeFilter = searchParams.get("type");
+   
+   const displayedVans = typeFilter 
+       ? vans.filter(van => van.type.toLowerCase() === typeFilter)
+       : vans
 
     useEffect(() => {
        fetch("/api/vans")
@@ -16,7 +22,7 @@ export default function Vans(){
         <>
         <h1 className="text-5xl p-4 font-semibold text-center">Explore our van options 🚐</h1>
             <article className="mt-10 mb-5 grid grid-cols-2 md:grid-cols-3 place-items-center gap-6 bg-secondary p-10">
-                {vans && vans.map(van => {
+                {displayedVans && displayedVans.map(van => {
                     return <Link key={van.id} to={`/vans/${van.id}`}>
                                 <Van vans={van}/> 
                            </Link>
