91bd1bbd3800eb7dde40b05b0d1b763c0b20306b
Junior Hirwa <iamhirwejr@gmail.com>
2023-10-10 11:38:18 +0200

    style: update the filters

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -22,9 +22,18 @@ export default function Vans(){
         <>
         <h1 className="text-5xl p-4 font-semibold text-center">Explore our van options 🚐</h1>  
         <div className="ml-3">
-            <button onClick={()=> setSearchParams({type: "simple"})} className="p-2 ml-1 bg-white font-semibold rounded-md hover:bg-primary hover:text-white hover:cursor-pointer">Simple</button>
-            <button onClick={()=> setSearchParams({type: "rugged"})} className="p-2 ml-1 bg-white font-semibold rounded-md hover:bg-darkGreen hover:text-white hover:cursor-pointer">Rugged</button>
-            <button onClick={()=> setSearchParams({type: "luxury"})} className="p-2 ml-1 bg-white font-semibold rounded-md hover:bg-black hover:text-white hover:cursor-pointer">Luxury</button>
+            <button onClick={()=> setSearchParams({type: "simple"})} 
+                    className={typeFilter === "simple" 
+                    ? "p-2 ml-1 bg-primary text-white font-semibold rounded-md hover:bg-primary hover:text-white hover:cursor-pointer"
+                    :"p-2 ml-1 bg-white font-semibold rounded-md hover:bg-primary hover:text-white hover:cursor-pointer"}>Simple</button>
+            <button onClick={()=> setSearchParams({type: "rugged"})} 
+            className={typeFilter === "rugged" 
+            ? "p-2 ml-1 bg-darkGreen text-white font-semibold rounded-md hover:bg-darkGreen hover:text-white hover:cursor-pointer"
+            :"p-2 ml-1 bg-white font-semibold rounded-md hover:bg-darkGreen hover:text-white hover:cursor-pointer"}>Rugged</button>
+            <button onClick={()=> setSearchParams({type: "luxury"})} 
+                    className={typeFilter === "luxury" 
+                    ? "p-2 ml-1 bg-black text-white font-semibold rounded-md hover:bg-black hover:text-white hover:cursor-pointer"
+                    :"p-2 ml-1 bg-white font-semibold rounded-md hover:bg-black hover:text-white hover:cursor-pointer"}>Luxury</button>
             {typeFilter && <button onClick={()=> setSearchParams({})} className="p-2 ml-1 bg-white font-semibold rounded-md hover:bg-darkGray hover:text-white hover:cursor-pointer">Clear Filter</button>}
         </div>
             <article className="mt-10 mb-5 grid grid-cols-1 md:grid-cols-3 place-items-center gap-6 bg-secondary p-10">
