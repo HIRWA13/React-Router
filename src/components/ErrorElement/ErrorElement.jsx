03ef5d96609fb4e47cd1e2807d1817ce060ad9f0
Junior Hirwa <iamhirwejr@gmail.com>
2023-10-13 14:56:36 +0200

    feat: use the useRouterError

--- src/components/ErrorElement/ErrorElement.jsx
+++ src/components/ErrorElement/ErrorElement.jsx
@@ -7,7 +7,7 @@ const error = useRouteError()
         <section className="body-font flex flex-col gap-2 items-center justify-center mt-10">
             <h1 className="text-gray-600 text-5xl p-4 font-semibold text-center">Error</h1>
             <div>
-                <pre className="text-center text-2xl">there was an error</pre>
+                <pre className="text-center text-2xl">there was an error {error.message}</pre>
             </div>
             <h1 className="text-center text-white bg-black w-1/4 p-2 rounded-md">Try again later</h1>
         </section>
