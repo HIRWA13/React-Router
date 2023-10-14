40d208d3b96ec53ace9f4a3df5b0ecca1ec964cb
Junior Hirwa <iamhirwejr@gmail.com>
2023-10-14 15:27:37 +0200

    feat: update the isLoggedIn to true and add the function return

--- utils.js
+++ utils.js
@@ -1,11 +1,12 @@
 import { redirect } from "react-router-dom"
 
 export async function requireAuth() {
-    const isLoggedIn = false
+    const isLoggedIn = true
     
     if (!isLoggedIn) {
-       const re= redirect("/login")
+       const re= redirect("/login?message= Please login to continue")
        re.body=true
        throw re
     } 
+    return null
 }
