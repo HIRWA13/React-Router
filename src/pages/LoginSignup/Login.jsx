19170a0a5617d993059fec263936c6d7a7bc5076
Junior Hirwa <iamhirwejr@gmail.com>
2023-10-16 08:33:33 +0200

    feat: update the form to use actions and the Form component plus working with local storage

--- src/pages/LoginSignup/Login.jsx
+++ src/pages/LoginSignup/Login.jsx
@@ -1,55 +1,74 @@
 import { useState } from "react";
-import { Navigate, useLoaderData, useNavigate } from "react-router-dom";
+import { useLoaderData, useNavigate, Form } from "react-router-dom";
 import { loginUser } from "../../../api";
 
-export const loader = ({request}) => {
-  return new URL(request.url).searchParams.get("message")
-}
+export const loader = ({ request }) => {
+  return new URL(request.url).searchParams.get("message");
+};
+
+export const action = async ({request}) => {
+    const formData = await request.formData();
+    const email = formData.get("email");
+    const password = formData.get("password")
+    const data = await loginUser({email, password})
+    localStorage.setItem("login", true)
+    console.log(data);
+    return null
+};
 
 export default function Login() {
-  const [loginFormData, setLoginFormData] = useState({ email: "", password: "" })
-  const [status, setStatus] = useState("idle")
-  const [error, setError] = useState(null)
+  const [loginFormData, setLoginFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const [status, setStatus] = useState("idle");
+  const [error, setError] = useState(null);
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-    const  handleSubmit = (e)=> {
-        e.preventDefault()
-        setStatus("submitting")
-        setError(null)
-        loginUser(loginFormData)
-            .then(data => {
-              navigate("/host", {replace: true})
-            })
-            .catch(err => setError(err))
-            .finally(() => setStatus("idle"))
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setStatus("submitting");
+    setError(null);
+    loginUser(loginFormData)
+      .then((data) => {
+        navigate("/host", { replace: true });
+      })
+      .catch((err) => setError(err))
+      .finally(() => setStatus("idle"));
+  };
 
-    function handleChange(e) {
-        const { name, value } = e.target
-        setLoginFormData(prev => ({
-            ...prev,
-            [name]: value
-        }))
-    }
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setLoginFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  }
   const message = useLoaderData();
   return (
     <>
       <section>
         <div className="w-full min-h-screen bg-gray-50 flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
           <div className="w-full sm:max-w-md p-5 mx-auto">
-            <h2 className="mb-12 text-center text-5xl font-extrabold">Sign in to your account</h2>
-            {error && <p className="text-red-500 text-center mb-4">{error.message}</p>}
-            {message && <p className="text-red-500 text-center mb-4">{message}</p>}
-            <form onSubmit={handleSubmit}>
+            <h2 className="mb-12 text-center text-5xl font-extrabold">
+              Sign in to your account
+            </h2>
+            {error && (
+              <p className="text-red-500 text-center mb-4">{error.message}</p>
+            )}
+            {message && (
+              <p className="text-red-500 text-center mb-4">{message}</p>
+            )}
+            <Form method="post">
               <div className="mb-4">
                 <label className="block mb-1"> Email-Address</label>
                 <input
                   id="email"
                   type="text"
                   name="email"
-                  onChange={handleChange}
-                  value={loginFormData.email}
+                  // onChange={handleChange}
+                  // value={loginFormData.email}
                   className="py-2 px-3 border border-gray-300 focus:border-red-300 focus:outline-none focus:ring focus:ring-red-200 focus:ring-opacity-50 rounded-md shadow-sm disabled:bg-gray-100 mt-1 block w-full"
                 />
               </div>
@@ -59,8 +78,8 @@ export default function Login() {
                   id="password"
                   type="password"
                   name="password"
-                  onChange={handleChange}
-                  value={loginFormData.password}  
+                  // onChange={handleChange}
+                  // value={loginFormData.password}
                   className="py-2 px-3 border border-gray-300 focus:border-red-300 focus:outline-none focus:ring focus:ring-red-200 focus:ring-opacity-50 rounded-md shadow-sm disabled:bg-gray-100 mt-1 block w-full"
                 />
               </div>
@@ -71,9 +90,10 @@ export default function Login() {
                 </a>
               </div>
               <div className="mt-6">
-                <button 
-                disabled={status === "submitting"}
-                className="w-full inline-flex items-center justify-center px-4 py-2 bg-primary border border-transparent rounded-md font-semibold capitalize text-white hover:bg-red-700 active:bg-red-700 focus:outline-none focus:border-red-700 focus:ring focus:ring-red-200 disabled:opacity-25 transition">
+                <button
+                  disabled={status === "submitting"}
+                  className="w-full inline-flex items-center justify-center px-4 py-2 bg-primary border border-transparent rounded-md font-semibold capitalize text-white hover:bg-red-700 active:bg-red-700 focus:outline-none focus:border-red-700 focus:ring focus:ring-red-200 disabled:opacity-25 transition"
+                >
                   {status === "submitting" ? "Logging..." : "Sign In"}
                 </button>
               </div>
@@ -82,7 +102,7 @@ export default function Login() {
                   Sign up for an account
                 </a>
               </div>
-            </form>
+            </Form>
           </div>
         </div>
       </section>
