822c6404d0e0d0d62ac3454f2971c0d5109e3435
Junior Hirwa <iamhirwejr@gmail.com>
2023-11-14 11:22:13 +0200

    feat: add nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,20 @@ import Contact from "./components/Contact";
 import NavBar from "./components/NavBar";
 import OrderSummary from "./components/OrderSummary";
 import NotFound from "./components/NotFound";
+import Products from "./components/Products";
+import FeaturedProducts from "./components/FeaturedProducts";
+import NewProducts from "./components/NewProducts";
 
 function App() {
   return (
     <>
-    <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
+        <Route path="products" element={<Products />}>
+          <Route path="featured" element={<FeaturedProducts />} />
+          <Route path="new" element={<NewProducts />} />
+        </Route>
         <Route path="contact" element={<Contact />} />
         <Route path="order-summary" element={<OrderSummary />} />
         <Route path="*" element={<NotFound />} />
