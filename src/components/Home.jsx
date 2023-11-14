2c865e4bfd1811545f5940dd78195a37bd5a84dd
Junior Hirwa <iamhirwejr@gmail.com>
2023-11-14 11:21:34 +0200

    feat: update the home route

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,12 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Outlet } from "react-router-dom";
+import NavBar from "./NavBar";
 
 export default function Home() {
   const navigate = useNavigate();
   return (
     <>
+    <NavBar />
+    <Outlet />
       <div className="text-xl font-bold">Home Page</div>
       <button onClick={() => navigate('order-summary')} className="py-2 px-6 bg-gray-400 w-44">place your order</button>
     </>
