091f037d3a58d26abcabc430db1287707b6ae9ac
Junior Hirwa <iamhirwejr@gmail.com>
2023-11-17 09:10:07 +0200

    feat: users and intro to dynamic routes

---
+++ src/components/Users.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom"
+
+export default function Users() {
+  return (
+    <>
+      <div>
+        <Link to="1" className="text-sm m-3 underline">
+          User 1
+        </Link>
+        <Link to="2" className="text-sm m-3 underline">
+          User 2
+        </Link>
+        <Link to="3" className="text-sm m-3 underline">
+          User 3
+        </Link>
+      </div>
+    </>
+  );
+}
