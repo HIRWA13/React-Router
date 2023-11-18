b794081e9e3810a3457c0a3c5eb874eccf76b665
Junior Hirwa <iamhirwejr@gmail.com>
2023-11-18 11:32:28 +0200

    feat: url params with react router

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -1,7 +1,11 @@
+import { useParams } from "react-router-dom";
+
 export default function UserDetails() {
+  const params = useParams();
+  const userId = params.userId;
   return (
     <div className="text-lg text-center font-bold">
-        user Details goes  here
+        detais for user {userId}
     </div>
   )
 }
